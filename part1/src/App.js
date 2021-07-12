f3432bf2d39517a9ae6405624ae31def7d4b05d1
aw zhen yi <garythebob@gmail.com>
2021-07-12 22:07:39 +0800

    Done 1.3

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,8 +1,49 @@
 import React from 'react'
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = ({ name, exercises }) => <p>{name} {exercises}</p>
+
+const Content = ({ partsData }) => <>
+    {partsData.map(p => <Part key={p.name} {...p} />)}
+</>
+
+
+const Total = (props) => {
+
+  return (
+    <p> Number of exercises: {props.total} </p>
+
+  )
+}
+
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course = {course}/>
+      <Content partsData={[part1, part2, part3,]} />
+      <Total total = {part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  )
+}
 
 export default App

c2b6032de8c2be0314385d7ba856e6c3703be4bc
aw zhen yi <garythebob@gmail.com>
2021-08-07 19:24:02 +0800

    done 1.8

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,8 +1,21 @@
-import { directive } from '@babel/types';
 import React, { useState } from 'react';
 
-function App() {
+const Statistics = (props) => {
+  const { good, neutral, bad, all } = props;
+  return (
+    <div>
+      <h2>statistics</h2>
+      <p>Good: {good} </p>
+      <p>Neutral: {neutral} </p>
+      <p>Bad: {bad} </p>
+      <p>All: {all}</p>
+      <p>Average: {(good - bad) / all} </p>
+      <p>Positive: {good / all * 100}  %</p>
+    </div>
+  )
+}
 
+const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
@@ -14,15 +27,7 @@ function App() {
       <button onClick={() => setGood(good + 1)}>good</button>
       <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
       <button onClick={() => setBad(bad + 1)}>bad</button>
-      <div>
-        <h2>statistics</h2>
-        <p>Good: {good} </p>
-        <p>Neutral: {neutral} </p>
-        <p>Bad: {bad} </p>
-        <p>All: {all}</p>
-        <p>Average: {(good - bad) / all} </p>
-        <p>Positive: {good / all * 100}  %</p>
-      </div>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all}/>
     </div>
   );
 }

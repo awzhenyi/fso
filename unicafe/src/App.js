54003efccfeb24b1edbccba7c6d3e8814bf5155d
aw zhen yi <garythebob@gmail.com>
2021-08-07 19:11:52 +0800

    done 1.7

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const all = good + neutral + bad;
 
   return (
     <div>
@@ -18,6 +19,9 @@ function App() {
         <p>Good: {good} </p>
         <p>Neutral: {neutral} </p>
         <p>Bad: {bad} </p>
+        <p>All: {all}</p>
+        <p>Average: {(good - bad) / all} </p>
+        <p>Positive: {good / all * 100}  %</p>
       </div>
     </div>
   );

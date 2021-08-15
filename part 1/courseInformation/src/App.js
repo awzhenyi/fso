5b2b31f02d5828b2b3ea3523992fae1d515c318f
aw zhen yi <garythebob@gmail.com>
2021-08-15 16:46:22 +0800

    Done 2.5

--- part 1/courseInformation/src/App.js
+++ part 1/courseInformation/src/App.js
@@ -1,51 +1,5 @@
 import React from 'react'
-
-const Header = ( {name} ) => {
-  return (
-    <h1>{ name }</h1>
-  )
-}
-
-const Part = ( {partsName, partsExercises} ) => {
-  return (
-    <p> {partsName} {partsExercises} </p>
-  )
-}
-
-const Content = ( {parts} ) => {
-  return (
-    <div>
-      {parts.map(part =>
-        <div key={part.id}>
-          <Part partsName={part.name} partsExercises={part.exercises}/>
-        </div>
-      )}
-    </div>
-  )
-}
-
-
-const Total = ( {parts} ) => {
-  return (
-    <p> 
-    Number of exercises: {parts.reduce((accumulator, currentValue) => (accumulator + currentValue.exercises), 0)}
-    </p>
-  )
-}
-
-const Course = ( {courses} ) => {
-  return (
-    <div>
-      {courses.map(course => 
-        <div key={course.id}>
-        <Header name={course.name}/>
-        <Content parts={course.parts}/>
-        <Total parts={course.parts}/>
-        </div>
-      )}
-    </div>
-  )
-}
+import Course from './Components/Course'
 
 const App = () => {
   const courses = [

78ce231145adad7898e4a6561a5540e29c3fe762
aw zhen yi <garythebob@gmail.com>
2021-08-16 23:49:41 +0800

    Done 2.6-2.8

--- part 2/phonebook/src/App.js
+++ part 2/phonebook/src/App.js
@@ -1,51 +1,87 @@
 import React, { useState } from 'react'
-import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
-  const [newNote, setNewNote] = useState('a new note....')
-  const [showAll, setShowAll] = useState(true)
-
-  const addNote = (event) => {
-    event.preventDefault()
-    const noteObject = {
-      content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1,
+const App = () => {
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas',
+      number: '91234567'
     }
-  
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
-  }
-  
-  const handleNoteChange = (event) => {
-    console.log(event.target.value)
-    setNewNote(event.target.value)
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newFilter, setNewFilter ] = useState('')
+
+  const filterPerson = (event) => {
+    event.preventDefault();
+    for(const person of persons) {
+      if (person.name === newFilter) {
+        console.log('exists')
+      }
+    }
+
+    setNewFilter('')
   }
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personToAdd = {
+      name : newName,
+      number : newNumber,
+    }
+
+    if (persons.some((person) => (person.name===personToAdd.name && person.number===personToAdd.number))) {
+      alert(`${newName} is already added to the phonebook`)
+      return
+    }
+
+    setPersons(persons.concat(personToAdd));
+    setNewName('');
+    setNewNumber('');
+  }
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value)
+  }
 
   return (
     <div>
-      <h1>Notes</h1>
+      <h2>Phonebook</h2>
       <div>
-        <button onClick={()=>setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'every thing'}
-        </button>
+        <h2>Filter By</h2>
+        <form onSubmit={filterPerson}>
+          <input 
+            value = {newFilter}
+            onChange = {handleFilterChange}
+          />
+        </form>
       </div>
-      <ul>
-        {notesToShow.map(note => 
-            <Note key={note.id} note={note} />
-        )}
-      </ul>
-      <form onSubmit={addNote}>
-        <input 
-          value = {newNote}
-          onChange = {handleNoteChange}
-        />
-        <button type="submit">save</button>
+      <form onSubmit={addPerson}>
+        <div>
+        <h2>Add New Person</h2>
+          name: <input 
+            value = {newName}
+            onChange = {handleNameChange}
+          />
+          <div>
+            number: <input 
+              value = {newNumber}
+              onChange = {handleNumberChange}
+            />
+          </div>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
       </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => 
+        <p>{person.name} {person.number}</p>
+      )}
     </div>
   )
 }

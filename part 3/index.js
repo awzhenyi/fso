a60941adcda15a9270b94bb6e945d99f966fb089
awzhenyi <garythebob@gmail.com>
2022-03-07 22:27:11 +0800

    Part 3 changes

--- part 3/index.js
+++ part 3/index.js
@@ -1,33 +1,81 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 
-let notes = [
+let persons = [
     {
-      id: 1,
-      content: "HTML is easy",
-      date: "2019-05-30T17:30:31.098Z",
-      important: true
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
     },
     {
-      id: 2,
-      content: "Browser can execute only Javascript",
-      date: "2019-05-30T18:39:34.091Z",
-      important: false
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
     },
     {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      date: "2019-05-30T19:20:14.298Z",
-      important: true
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
     }
-  ] 
+]
 
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
 })
 
-app.get('/api/notes', (request, response) => {
-  response.json(notes)
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter((person) => person.id !== id)
+  response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(person => person.id))
+    : 0
+  const person = request.body
+
+  if(!person.content) {
+    return response.status(400).json({
+      error: "missing content"
+    })
+  }
+
+  if(persons.filter((p) => p.name === person.name).length > 0) {
+    return response.status(400).json({
+      error: "name already exists, must be unique"
+    })
+  }
+
+  person.id = Math.floor(Math.random() * 10 + maxId)
+  persons = [...persons, person]
+  response.json(person)
+})
+
+app.get('/info', (request, response) => {
+  const currentDate = new Date().toLocaleString();
+  response.send(
+    `<div>
+      <p>Phonebook has info for ${persons.length} people</p>
+      <p>${currentDate}</p> 
+    </div>`)
 })
 
 const PORT = 3001
